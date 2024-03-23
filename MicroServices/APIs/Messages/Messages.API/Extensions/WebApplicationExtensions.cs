33d65e0dcce24f7328504145c87bd159c96e859f
hingel <henrik.ingelsten@iths.se>
2024-03-23 10:34:20 +0100

    - Added Ocelot data for Messages api

--- MicroServices/APIs/Messages/Messages.API/Extensions/WebApplicationExtensions.cs
+++ MicroServices/APIs/Messages/Messages.API/Extensions/WebApplicationExtensions.cs
@@ -9,12 +9,12 @@ public static class WebApplicationExtensions
 {
 	public static WebApplication MapMessageEndPoints(this WebApplication app)
 	{
-		app.MapPost("/addMessage", async (IMediator mediator, MessageDto newMessage) =>
+		app.MapPost("/messages", async (IMediator mediator, [FromBody] MessageDto newMessage) =>
 			await mediator.Send(new AddMessageCommand(newMessage)));//.RequireAuthorization();
-		app.MapGet("/getUsersMessages", async (IMediator mediator, string userId) => await mediator.Send(new GetUsersMessages(Guid.Parse(userId)))); //.RequireAuthorization(););
-		app.MapDelete("/deleteMessage", async (IMediator mediator, string messageId) =>
-			await mediator.Send(new DeleteMessageCommand(Guid.Parse(messageId)))); //.RequireAuthorization();
-		app.MapPatch("/updateMessage", async (IMediator mediator, MessageDto messageToUpdate) =>
+		app.MapGet("/messages/{id}", async (IMediator mediator, string id) => await mediator.Send(new GetUsersMessages(Guid.Parse(id)))); //.RequireAuthorization(););
+		app.MapDelete("/messages/{id}", async (IMediator mediator, string id) =>
+			await mediator.Send(new DeleteMessageCommand(Guid.Parse(id)))); //.RequireAuthorization();
+		app.MapPatch("/messages", async (IMediator mediator, [FromBody] MessageDto messageToUpdate) =>
 			await mediator.Send(new UpdateMessageCommand(messageToUpdate))); //.RequireAuthorization();
 		return app;
 	}
